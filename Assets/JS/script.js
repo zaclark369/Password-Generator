e1f1392c3d2bfe48d138bcdfa514633d78c136b6
Zachary Clark <zacclark369@gmail.com>
2021-11-27 14:35:12 -0800

    javascript runs correctly

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -19,48 +19,127 @@
 // WHEN the password is generated
 // THEN the password is either displayed in an alert or written to the page
 
-const lowerCaseLetters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-const upperCaseLetter = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-const numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
-const charactersSpecial = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '+', '='];
-
-var choices = [
-  lowerCaseLetters, 
-  upperCaseLetter, 
-  numbers, 
-  charactersSpecial
+const lowerCaseLetters = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+const upperCaseLetter = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
+const numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+const charactersSpecial = [
+  "!",
+  "@",
+  "#",
+  "$",
+  "%",
+  "^",
+  "&",
+  "*",
+  "(",
+  ")",
+  "+",
+  "=",
 ];
 
+var choices = [lowerCaseLetters, upperCaseLetter, numbers, charactersSpecial];
 // select choices
 function selectChoice() {
-  var characters = [];
-  while (characters.length < 1) {
-    for (var i = 0; i < characters.length; i++) {
-      var userPrompt = confirm('Use ${choices}[i] in your password?');
-      if (userPrompt) {
-        for (var j = 0; j < characters.length; j++) {
-          characters.push(choices[i][j]);
-        }
-      }
-    }
-    if (characters.length < 1) {
-      alert('Please select characters to procede.');
+ var characters = [];
+  // while in the array less than the total of the array, run for loop
+  // while (characters.length < 1) {
+  // for each instance of the array until we hit the end of the array, ask would you like anything in the choices array in your password. Confirm OK for yes, cancel for no.
+  for (var i = 0; i < choices.length; i++) {
+    var userPrompt = confirm(`Use ${choices[i]} in your password?`);
+    if (userPrompt) {
+      characters = characters.concat(choices[i]);
+      console.log("insideIfStatement");
+      console.log(characters);
+      // for (var j = 0; j < characters.length; j++) {
+      //   characters.push(choices[i][j]);
+      // }
     }
   }
-  return characters;
+  // if (characters.length = 0) {
+  //   alert("Please select characters to procede.");
+  //   selectChoice();
+  // }
+  console.log(characters);
+   return characters;
 }
+
 
 // select password length
 function pwLength() {
   var passLength = parseInt(
-    prompt('How long would you like your password? We can take 8-128 characters.')
+    prompt(
+      "How long would you like your password? We can take 8-128 characters."
+    )
   );
   while (isNaN(passLength) || passLength < 8 || passLength > 128) {
-    passLength = parseInt(prompt('Please select a number between 8-128'));
+    passLength = parseInt(prompt("Please select a number between 8-128"));
   }
   return passLength;
 }
 
+function generatePassword() {
+  var length = pwLength();
+  console.log(length);
+  var possCharacters = selectChoice();
+  console.log(possCharacters);
+  var generatedpassword = [];
+  for (var i = 0; i < length; i++) {
+    generatedpassword.push(possCharacters[Math.floor(Math.random() * (possCharacters.length) )]);
+  }
+  return generatedpassword.join('');
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -71,11 +150,9 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
 
 // this is a test
